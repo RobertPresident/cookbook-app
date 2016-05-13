f448fa651708a8aa69f0544b0ee9ee0322c49381
Robert President <bigmike73184@gmail.com>
2016-05-12 21:27:09 -0500

    update1

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,41 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    
+  end
+
+  def create
+    @recipe = Recipe.create(
+      title: params[:title],
+      chef: params[:chef],
+      ingredients: params[:ingredients],
+      instructions: params[:instructions]
+      )
+
+    render 'show.html.erb'
+  end
+
+  def edit
+   @recipe = Recipe.find_by(id: params[:id]) 
+  end
+
+  def update
+   @recipe = Recipe.find_by(id: params[:id]) 
+
+   @recipe.update(
+      title: params[:title],
+      chef: params[:chef],
+      ingredients: params[:ingredients],
+      instructions: params[:instructions]
+      )
+
+   render 'show.html.erb'
+  end
 end
